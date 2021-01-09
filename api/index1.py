736947dae20215b483910c3d05704923a88dcc3a
Ayan Mullick <ayan.mullick@tcs.com>
2021-01-09 17:23:49 +0530

    change 5

--- api/index1.py
+++ api/index1.py
@@ -1,11 +1,20 @@
-from http.server import BaseHTTPRequestHandler
+# from http.server import BaseHTTPRequestHandler
 
-class handler(BaseHTTPRequestHandler):
+# class handler(BaseHTTPRequestHandler):
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        message = 'Ayan Mullick 123'
-        self.wfile.write(message.encode())
-        return
+#     def do_GET(self):
+#         self.send_response(200)
+#         self.send_header('Content-type','text/plain')
+#         self.end_headers()
+#         message = 'Ayan Mullick 123'
+#         self.wfile.write(message.encode())
+#         return
+
+
+from flask import Flask, Response
+app = Flask(__name__)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
